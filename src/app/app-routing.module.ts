5df3e58e4307f6719a8fc96f4cca74fe4fc94e49
hzjanger <1305696249@qq.com>
2019-10-23 10:26:47 +0800

    主模块

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {WarehouseComponent} from './page/warehouse/warehouse.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',component: WarehouseComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,43 @@
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule, DomSanitizer} from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { NavHeaderComponent } from './page/nav-header/nav-header.component';
+import {SharedModule} from './shared/shared.module';
+import {ReactiveFormsModule} from '@angular/forms';
+import { WarehouseComponent } from './page/warehouse/warehouse.component';
+import { DetailedInformationComponent } from './page/warehouse/detailed-information/detailed-information.component';
+import {MatIconRegistry} from '@angular/material';
+import {loadSvgResource} from './utils/svg.utils';
+import {HttpClientModule} from '@angular/common/http';
+import { FooterComponent } from './page/footer/footer.component';
+import {WarehouseCardComponent} from './page/warehouse/warehouse-card/warehouse-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavHeaderComponent,
+    WarehouseCardComponent,
+    WarehouseComponent,
+    DetailedInformationComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+  constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) {
+    loadSvgResource(matIconRegistry, domSanitizer)
+  }
+}
