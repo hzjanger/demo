14fc06b03e3f92d612953d36ed2e1a71f522d706
hzjanger <1305696249@qq.com>
2019-10-23 10:24:00 +0800

    商品明细实体

---
+++ src/app/entity/detailed-information.ts
@@ -0,0 +1,15 @@
+export interface DetailedInformation {
+  img: string;
+  id: number;
+  name: string;
+  brand: string;
+  generalCategory: string;
+  medium: string;
+  small: string;
+  noticeNumber: number;
+  tagPrice: number;
+  tagAmount: number;
+  retailPlus: number;
+  retailAmount: number;
+  operating: string;
+}
